76d035dc96b82ecf05fde6543f95c1d52549991b
H-037 <122795604+H-037@users.noreply.github.com>
2023-01-31 10:47:52 +0100

    add playerSelection

--- code.js
+++ code.js
@@ -1,8 +1,24 @@
+// overall container //
+
 //display one of 3 variables (rock, paper, scissor)
-function getComputorChoice () {
-    const choice = ["Rock", "Paper", "Scissor"]
-    const random = Math.floor(Math.random() * choice.length);
-    console.log(random, choice[random]);
+function getComputorChoice() {
+    const cchoice = ["rock", "paper", "scissor"]
+    const random = Math.floor(Math.random() * cchoice.length);
+    console.log(random, cchoice[random]);
 }
 
-console.log(getComputorChoice());
+//display one of 3 by by player selected variables
+function playerSelection() {
+    let pchoice = prompt("Rock, Paper or Scissor?"); //ask for choice
+    let result = pchoice.toLowerCase(); //make Lowercase
+    console.log(result);
+}
+
+function game() {   //add both functions together
+    let sum = result + cchoice;
+    console.log(sum);
+}
+
+console.log(getComputorChoice(), playerSelection(), game());
+//add rules
+// return who wins and why
