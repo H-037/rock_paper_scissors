574c9031c0585d76aa014402af7466158e0eaca0
H-037 <122795604+H-037@users.noreply.github.com>
2023-01-31 10:08:32 +0100

    add getComputorChoice

--- code.js
+++ code.js
@@ -1,1 +1,8 @@
-console.log("Hello World"); 
+//display one of 3 variables (rock, paper, scissor)
+function getComputorChoice () {
+    const choice = ["Rock", "Paper", "Scissor"]
+    const random = Math.floor(Math.random() * choice.length);
+    console.log(random, choice[random]);
+}
+
+console.log(getComputorChoice());
