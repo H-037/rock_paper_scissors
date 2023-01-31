2912c1b4e7171da6f2be93d8ee99e98e153b3be1
H-037 <122795604+H-037@users.noreply.github.com>
2023-01-31 16:53:11 +0100

    add function rule and oneround together

--- code.js
+++ code.js
@@ -1,24 +1,42 @@
-// overall container //
-
 //display one of 3 variables (rock, paper, scissor)
 function getComputorChoice() {
     const cchoice = ["rock", "paper", "scissor"]
     const random = Math.floor(Math.random() * cchoice.length);
-    console.log(random, cchoice[random]);
+    return random, cchoice[random];
 }
 
 //display one of 3 by by player selected variables
 function playerSelection() {
     let pchoice = prompt("Rock, Paper or Scissor?"); //ask for choice
     let result = pchoice.toLowerCase(); //make Lowercase
-    console.log(result);
+    return result;
 }
 
-function game() {   //add both functions together
-    let sum = result + cchoice;
-    console.log(sum);
+//compare string with rules and make output
+function rules() {
+    let a = playerSelection();
+    let b = getComputorChoice();
+    let string = a+b;
+    if (string ==="paperpaper" || string ==="rockrock" || string ==="scissorscissor") {
+        return "Its a Tie! Try again.";
+    }
+    if (string ==="rockscissor") {
+        return "You win! Rock beat\’s scissor";
+    }
+    if (string ==="paperrock") {
+        return "You win! Paper beat\’s rock";
+    }
+    if (string ==="scissorpaper") {
+        return "You win! Scissor beat\’s paper";
+    }
+    if (string ==="scissorrock") {
+        return "You lose! Rock beat\’s scissor";
+    }
+    if (string ==="rockpaper") {
+        return "You lose! Paper beat\’s rock";
+    }
+    if (string ==="paperscissor") {
+        return "You lose! Scissor beat\’s paper";
+    }
 }
-
-console.log(getComputorChoice(), playerSelection(), game());
-//add rules
-// return who wins and why
+console.log(rules());
