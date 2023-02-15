55cded65e24d799838dd4eb2a4590e4299a28fc0
H-037 <122795604+H-037@users.noreply.github.com>
2023-02-15 14:40:29 +0100

    making for every choice extra gamefunction

--- code.js
+++ code.js
@@ -12,26 +12,43 @@ function getComputorChoice() {
     return random, cchoice[random];
 }
 
-//display one of 3 by by player selected variables
-function playerSelection() {
-    let pchoice = prompt("Rock, Paper or Scissor?"); //ask for choice
-    let result = pchoice.toLowerCase(); //make Lowercase
-    return result;
-}
-
-//compare string with rules and make output
-function rules() {
-    let a = playerSelection();
+//playing game with playerchoice = rock
+function prock() {
+    let a = "rock";
     let b = getComputorChoice();
     let string = a+b;
-    if (string ==="paperpaper" || string ==="rockrock" || string ==="scissorscissor") {
+    if (string ==="rockrock") {
         return "Its a Tie! Try again.";
     }
     if (string ==="rockscissor") {
         return "You win! Rock beat\’s scissor";
     }
+    if (string ==="rockpaper") {
+        return "You lose! Paper beat\’s rock";
+    }
+}
+
+function ppaper() {
+    let a = paper;
+    let b = getComputorChoice();
+    let string = a+b;
+    if (string ==="paperpaper") {
+        return "Its a Tie! Try again.";
+    }
     if (string ==="paperrock") {
         return "You win! Paper beat\’s rock";
+    }
+    if (string ==="paperscissor") {
+        return "You lose! Scissor beat\’s paper";
+    }
+}
+
+function pscissor() {
+    let a = "scissor";
+    let b = getComputorChoice();
+    let string = a+b;
+    if (string ==="scissorscissor") {
+        return "Its a Tie! Try again.";
     }
     if (string ==="scissorpaper") {
         return "You win! Scissor beat\’s paper";
@@ -39,15 +56,9 @@ function rules() {
     if (string ==="scissorrock") {
         return "You lose! Rock beat\’s scissor";
     }
-    if (string ==="rockpaper") {
-        return "You lose! Paper beat\’s rock";
-    }
-    if (string ==="paperscissor") {
-        return "You lose! Scissor beat\’s paper";
-    }
 }
 
 //showing Result
 const result = document.createElement('div');
-result.textContent = rules();
+result.textContent = prock();
 container.appendChild(result);
